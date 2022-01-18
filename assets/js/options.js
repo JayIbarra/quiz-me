d7b8c1165d69c752c9f484f2ab366d0c12bdf2d7
jay ibarra <jayibarra@hotmail.com>
2022-01-17 23:07:42 -0500

    updated

---
+++ assets/js/options.js
@@ -0,0 +1,27 @@
+//* * * *~ START OF QUESTIONS ~* * * * //
+// to list the options of answers, questions 
+
+// question #1 //
+var questions = [
+    {
+    title: "What does HTML stand for?", 
+    options: ["home tool markup langage", "hyperlinks and text markup language", "hyper total mongo language"],
+    answer: "hyper text markup language"
+},
+
+// question #2 //
+
+    {
+    title: "Arrays in JavaScript can be used to store ____.", 
+    options: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+    answer: "all of the above"
+},
+
+// question #3 //
+
+{
+    title: "String values must be enclosed within ___ when being assigned to variables.", 
+    options: ["commas", "curly brackets", "quotes", "parentheses"],
+    answer: "quotes"
+    }
+];
